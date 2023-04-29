bf6264a5b2e59e604ec25aa3da609aaed1921b0d
Osman Pérez <operez@edutecno.com>
2023-04-29 13:51:45 -0400

    Comentarios para cada una de las funcionalidades

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
+// Clase constructora para instanciar objetos de Bebidas
 function Bebida(nombre, precio) {
     this.nombre = nombre
     this.precio = precio
     this.seleccionado = false
 }
 
+// Clase constructora para instanciar objetos de Menú
 function Menu(nombre, precio, descripcion, foto) {
     this.nombre = nombre
     this.precio = precio
@@ -12,7 +14,7 @@ function Menu(nombre, precio, descripcion, foto) {
     this.seleccionado = false
 }
 
-// Objetos parra crear bebidas
+// Objetos para crear listado de bebidas
 var bebida1 = new Bebida("Martini", 2550)
 var bebida2 = new Bebida("Cappuccino", 1370)
 var bebida3 = new Bebida("Latte", 1350)
@@ -21,11 +23,9 @@ var bebida4 = new Bebida("Mojito", 2290)
 //Incorpora los objetos de bebidas en un arreglo
 var listadoBebidas = [ bebida1, bebida2, bebida3, bebida4 ]
 
-console.log(listadoBebidas);
 
 
-
-
+// Objetos para crear listado de Menú
 var menu1 = new Menu("Insalata de riso", 6750, "Ensalada para dos", "https://www.allrecipes.com/thmb/DnJpkLhBw09h2bQHnj-3_klYQSs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/3924402-fde39bbdca814afc878c7938d37c7c76.jpg")
 var menu2 = new Menu("Insalata al cipollotti", 5990, "Ensalada para uno", "https://www.cucchiaio.it/content/cucchiaio/it/ricette/2009/12/ricetta-insalata-ceci-patate-cipollotti/_jcr_content/header-par/image_single.img.jpg/1410277015032.jpg")
 var menu3 = new Menu("Insalata Caprese", 8250, "Ensala para uno con oregano", "https://www.recetasjudias.com/wp-content/uploads/2016/01/Ensalada-Caprese2.jpg")
@@ -36,14 +36,16 @@ var listadoMenu = []
 listadoMenu.push(menu1, menu2, menu3)
 
 
+//Función utilizada para detectar cuando el usuario hace click en el chec de una bebida
 function cambioBebida(indice, checkbox) {
-    var seleccionado = $(checkbox).prop('checked')
-    listadoBebidas[indice].seleccionado = seleccionado
+    var seleccionado = $(checkbox).prop('checked') // Consulta si el check que disparó el evento está checked o no
+    listadoBebidas[indice].seleccionado = seleccionado // Cambia en el arreglo de objetos la propiedad seleccionado al item de bebida que corresponde
     console.log(listadoBebidas);
 }
 
 
 $(document).ready(function(){
+    //Ciclo que muestra al usuario los items de bebidas 
     listadoBebidas.forEach((bebida, index) => {
         $("#listado-bebidas").append(`
             <li class="list-group-item d-flex justify-content-between">
@@ -64,6 +66,7 @@ $(document).ready(function(){
     })
 
 
+    //Ciclo que muestra al usuario los items de Menú
     listadoMenu.forEach((menu, index) => {
         $("#listado-menu").append(`
             <li class="list-group-item">
